f5252ebd7d691be043d49f68a2b0355a159f21de
miverson <miverson@vmware.com>
2022-10-08 19:40:51 -0700

    Spring App Initialized:

---
+++ src/test/java/com/morganiverson/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morganiverson.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
